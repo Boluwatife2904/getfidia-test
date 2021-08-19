79a32cb29b40c02946a639b1ec1f0731f051b1cd
Boluwatife2904 <sanvicola2000@gmail.com>
2021-08-19 16:12:17 +0100

    Added links to the home page to redirect to the main pages, and also added toggle button in sidebar

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -88,6 +88,13 @@
           Developers
         </router-link>
       </li>
+      <li class="toggle">
+        <div class="switch">
+          <input type="checkbox" name="test" id="test" />
+          <span class="slider round"></span>
+        </div>
+        <label for="test">View test data</label>
+      </li>
       <li>
         <router-link :to="{ name: 'Home' }">
           <img src="@/assets/images/settings-icon.svg" alt="settings-icon" />
@@ -195,6 +202,81 @@ export default {
 
   @media screen and (max-width: 1200px) {
     left: 100%;
+  }
+}
+
+// CUSTOM TOGGLE
+.toggle {
+  display: flex;
+  align-items: center;
+  gap: 6px;
+
+  label {
+    cursor: pointer;
+    font-size: 14px;
+    line-height: 17px;
+    color: #1a1f36;
+  }
+
+  .switch {
+    position: relative;
+    display: inline-block;
+    width: 26px;
+    height: 16px;
+
+    input {
+      opacity: 0;
+      width: 0;
+      height: 0;
+      outline: none;
+
+      &:focus {
+        outline: none;
+      }
+    }
+
+    .slider {
+      position: absolute;
+      cursor: pointer;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background: #e3e8ee;
+      -webkit-transition: 0.4s;
+      transition: 0.4s;
+
+      &::before {
+        position: absolute;
+        content: "";
+        height: 14px;
+        width: 14px;
+        left: 0px;
+        top: 0px;
+        background-color: white;
+        border: 1px solid #c2c7cf;
+        -webkit-transition: 0.4s;
+        transition: 0.4s;
+      }
+    }
+  }
+
+  input:checked + .slider {
+    background-color: var(--purpleColor);
+  }
+
+  input:checked + .slider:before {
+    -webkit-transform: translateX(12px);
+    -ms-transform: translateX(12px);
+    transform: translateX(12px);
+  }
+
+  .slider.round {
+    border-radius: 26px;
+  }
+
+  .slider.round:before {
+    border-radius: 50%;
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,68 @@
 <template>
-<h1>home</h1>
+  <div class="page-wrapper">
+    <div class="container">
+      <h1>Welcome</h1>
+      <p>
+        Below, you will find the links to the two pages in the Figma design of
+        the task attached in the job opening.
+      </p>
+      <div class="links">
+        <router-link :to="{ name: 'Login' }">Login</router-link>
+        <router-link :to="{ name: 'Payments' }">Payments</router-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Home',
-}
+  name: "Home",
+};
 </script>
+
+<style lang="scss" scoped>
+.page-wrapper {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #f1f1f1;
+
+  .container {
+    max-width: 500px;
+    width: 90%;
+    background: #fff;
+    box-shadow: 0 2px 4px 8px rgba(0, 0, 0, 0.02);
+    border-radius: 10px;
+    padding: 30px;
+    text-align: center;
+
+    h1 {
+      font-size: 24px;
+      margin-bottom: 10px;
+    }
+
+    p {
+      color: var(--gray);
+      font-size: 14px;
+      line-height: 24px;
+    }
+
+    .links {
+      margin: 30px 0;
+
+      a {
+        text-decoration: none;
+        border: none;
+        outline: none;
+        color: #fff;
+        background: var(--purpleColor);
+        font-size: 14px;
+        border-radius: 4px;
+        padding: 10px 16px;
+        margin: 10px;
+      }
+    }
+  }
+}
+</style>
